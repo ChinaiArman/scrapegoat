f2520b16d5a0b84759566c2de39253343e93041a
ChinaiArman <chinaiarman@gmail.com>
2025-12-01 03:24:24 -0800

    Update block.py

--- scrapegoat-core/src/scrapegoat_core/classes/block.py
+++ scrapegoat-core/src/scrapegoat_core/classes/block.py
@@ -3,9 +3,15 @@
 
 class GoatspeakBlock:
     """
+    A block of goatspeak commands, including a fetch command and a list of queries to be executed on the fetched HTML. These commands are to be executed together as a unit to complete the querying process.
     """
-    def __init__(self, fetch_command, query_list):
+    def __init__(self, fetch_command: "FetchCommand", query_list: list["Query"]): # type: ignore
         """
+        Initializes the GoatspeakBlock.
+
+        Args:
+            fetch_command (FetchCommand): The command to fetch HTML content.
+            query_list (list): A list of Query objects to be executed on the fetched HTML.
         """
         self.fetch_command = fetch_command
         self.query_list = query_list
@@ -14,18 +20,21 @@ class GoatspeakBlock:
         """
         """
         return f"GoatspeakBlock(fetch_command={self.fetch_command}, query_list={self.query_list})"
-    
-    def to_goat_file(self) -> None:
-        """
-        """
-        pass
 
 
 class Query:
     """
+    A query consisting of graze commands to be executed on an HTMLNode tree, along with optional fetch, churn, and deliver commands to control the data flow and representation.
     """
-    def __init__(self, graze_commands, fetch_command=None, churn_command=None, deliver_command=None):
+    def __init__(self, graze_commands: "GrazeCommand", fetch_command: "FetchCommand"=None, churn_command:"ChurnCommand"=None, deliver_command:"DeliverCommand"=None): # type: ignore
         """
+        Initializes the Query.
+
+        Args:
+            graze_commands (GrazeCommand): The graze commands to be executed.
+            fetch_command (FetchCommand): The command to fetch HTML content. Defaults to None.
+            churn_command (ChurnCommand): The command to extract data from scraped nodes. Defaults to None.
+            deliver_command (DeliverCommand): The command to deliver the results. Defaults to None.
         """
         self.fetch_command = fetch_command
         self.graze_commands = graze_commands
